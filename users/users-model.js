ee9108c1a6c56f989aadcbdc043d73fee1547b79
Vish Singh <vishalicious213@gmail.com>
2020-09-15 21:53:44 -0700

    users/users-model created

---
+++ users/users-model.js
@@ -0,0 +1,33 @@
+const db = require('../data/dbConfig')
+
+// GET all users
+function findAll() {
+	return db('users').select('id', 'name', 'email', 'city', 'state')
+}
+
+// ADD user
+async function add(user) {
+    const [id] = await db('users').insert(user)
+    return findById(id)
+}
+
+// FIND user by ID
+function findById(id) {
+    return db('users')
+        .select('id', 'name')
+        .where({ id })
+        .first()
+}
+
+// FIND user by filter (name, in this case)
+function findBy(filter) {
+	return db('users')
+		.select('id', 'name', 'email')
+		.where(filter)
+}
+
+module.exports = {
+    add,
+    findAll,
+    findBy,
+    findById
