7b4321f18bd8e6cc5d617b133a9cbb811f5a8f3b
Vish Singh <vishalicious213@gmail.com>
2020-09-17 11:00:18 -0700

    had wrong router paths in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
-const usersRouter = require('./routers/usersRouter');
-const issuesRouter = require('./routers/issuesRouter');
+const usersRouter = require('./users/usersRouter');
+const issuesRouter = require('./issues/issuesRouter');
 
 server.use(express.json());
 server.use('/users', usersRouter);
