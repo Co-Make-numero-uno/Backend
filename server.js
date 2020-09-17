d20bc1bbc7e4c7d61c24c0f9434557e259b8027b
Vish Singh <vishalicious213@gmail.com>
2020-09-17 16:50:34 -0700

    added CORS library and made server.use it

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+var cors = require('cors')
 const server = express();
 const usersRouter = require('./users/users-router');
 const issuesRouter = require('./issues/issues-router');
 
+server.use(cors());
 server.use(express.json());
 server.use('/users', usersRouter);
 server.use('/issues', issuesRouter);
