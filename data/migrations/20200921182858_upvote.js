a99b4fabcbd7c29202e49eaa162473f3ddc396fe
Jeff Glanville <jeff.glanville@outlook.com>
2020-09-21 19:29:06 -0500

    create upvote table

--- data/migrations/20200921182858_upvote.js
+++ data/migrations/20200921182858_upvote.js
@@ -1,10 +1,19 @@
 
 exports.up = async function(knex) {
   return knex.schema.createTable("upvote", (table) => {
-      table.increments();
+      table.integer("users_id")
+        .notNull()
+        .references("id")
+        .inTable("users")
+      table.integer("issues_id")
+        .notNull()
+        .references("id")
+        .inTable("issues")
+        .onDelete("SET NULL")
+      table.primary("users_id", "animal_id")
   })
 };
 
 exports.down = async function(knex) {
-  
+  await knex.schema.dropTaleIfExists("upvote")
 };
