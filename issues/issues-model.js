5e433905324061249e6872728c44226ce2050e92
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-25 21:26:34 -0400

    Implemented upvotes with JOIN

--- issues/issues-model.js
+++ issues/issues-model.js
@@ -2,8 +2,17 @@ const db = require('../data/dbConfig')
 
 // GET all issues
 function findAll() {
+    // return db.raw(`
+    //     SELECT issues.id, title, description, city, state, COUNT(upvote.id) AS votes FROM issues
+    //     LEFT JOIN upvote
+    //     ON upvote.issue_id = issues.id
+    //     GROUP BY issues.id
+    // `)
     return db('issues')
-        .select('issues.id', 'title', 'description', 'city', 'state', 'votes')
+        .select('issues.id', 'title', 'description', 'city', 'state',)
+        .leftJoin("upvote", "upvote.issue_id", "issues.id")
+        .count('upvote.id as votes')
+        .groupBy('issues.id');
 }
 
 // ADD issue
